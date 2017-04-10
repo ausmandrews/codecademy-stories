5e0020e9abb137be328c3f259f7332fea1998171
codecademy <ccuser@codecademy.com>
2017-04-10 06:05:18 +0000

    Add links to About Page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 	root "pages#home"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  get "/about" => "pages#about"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
